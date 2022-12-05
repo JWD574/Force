c6801c0ac1c8055ab9a41b46680c676c2d1eab78
Joseph DeWeese <joe.deweese@me.com>
2022-12-05 09:45:49 -0600

    Initial Commit

---
+++ Force/ForceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForceApp.swift
+//  Force
+//
+//  Created by Joseph  DeWeese on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ForceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
